c0f03ce5960fb4d26180b92d293940343523b7d5
lalit-143 <82702053+lalit-143@users.noreply.github.com>
2024-09-29 23:29:01 +0530

    Add files via upload

---
+++ gmiu_webapp/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(routes: {
+      "/": (_) => new WebviewScaffold(
+        url: "https://beta.gmgc.edu.in/",
+      )
+    });
+  }
+}
